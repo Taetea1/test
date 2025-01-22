7a87817f96c9508c517484d6eb73762a06a152dd
Taetae1123 <xodus2079@naver.com>
2025-01-23 00:59:42 +0900

    Fix: 수정 및 삭제 위치 오류 수정

--- 20250122/level3 copy.js
+++ 20250122/level3 copy.js
@@ -1,3 +1,4 @@
+// window.localStorage.clear();
 let isid = false;
 let isnick = false;
 let isage = false;
@@ -209,44 +210,49 @@ btn.addEventListener("click", () => {
     isbincheck.fill(0);
     btn.disabled = true;
   }
-}); //수정버튼
+});
 
 //수정
+let c3; //이름인풋
+let c2; //나이인풋
+let c; //커리어인풋
+let btnsss; //수정버튼
 let ischangecar = false;
 let ischangeage = false;
-let ischangename = false;
 const changeData = (id) => {
   const inputtest3 = document.querySelector(`.test3${id}`);
   const inputtest2 = document.querySelector(`.test2${id}`);
   const inputtest = document.querySelector(`.test${id}`);
-  let btnsss = document.getElementById(`${id}`);
+  btnsss = document.getElementById(`${id}`);
   if (btnsss.innerText === "수정") {
-    console.log(btnsss);
     btnsss.innerText = "수정완료";
     inputtest3.innerHTML = `
-    <input class="c c3${id}" oninput=checkChangename(${id}) value=${inputtest3.innerText} />`;
+    <input class="c c3${id}" value=${inputtest3.innerText} oninput=checkChangename(${id}) />`;
     inputtest2.innerHTML = `
-    <input type=number maxlength=3 oninput=numberMaxLength2(this) onkeyup=changeAGE(${id}) class="c c2${id}" value=${inputtest2.innerText} />
-    <div class="changecar changecar2${id}"></div>`;
+    <input type=number maxlength=3 oninput=numberMaxLength2(this) class="c c2${id}" value=${inputtest2.innerText} />
+    <div class="changecar2${id}"></div>`;
     inputtest.innerHTML = `
-    <input class="c c${id}" oninput=checkChangeCar(${id}) value=${inputtest.innerText}/>
-    <div class="changecar changecar${id}"></div>`;
+    <input class="c c${id}" value=${inputtest.innerText} oninput=checkChangeCar(${id}) />
+    <div class="changecar${id}"></div>`;
+
+    // 입력조건 확인하기 위해서
+    c3 = document.querySelector(`.c3${id}`);
+    c2 = document.querySelector(`.c2${id}`);
+    c = document.querySelector(`.c${id}`);
   } else if (
     btnsss.innerText === "수정완료" &&
     ischangecar === false &&
-    ischangeage === false &&
-    ischangename === false
+    ischangeage === false
   ) {
-    console.log(btnsss);
     btnsss.innerText = "수정";
 
     //직접변경
     let changecar = JSON.parse(localStorage.getItem("userInfo"));
 
     // 나중에 만들어줘야 각각을 알 수 있음
-    let c3 = document.querySelector(`.c3${id}`);
-    let c2 = document.querySelector(`.c2${id}`);
-    let c = document.querySelector(`.c${id}`);
+    c3 = document.querySelector(`.c3${id}`);
+    c2 = document.querySelector(`.c2${id}`);
+    c = document.querySelector(`.c${id}`);
     // 객체의 인덱스 찾기
     let index = changecar.findIndex((obj) => obj.id === `${id}`);
     changecar[index].name = c3.value;
@@ -254,119 +260,68 @@ const changeData = (id) => {
     changecar[index].car = c.value;
     localStorage.setItem(`userInfo`, JSON.stringify(changecar));
 
-    // 바뀐값 전역변수에 넣어주기
-    let userL = JSON.parse(localStorage.getItem("userInfo"));
-    data = [];
-    data.push(...userL);
-
+    // inputtest.innerHTML = `<td class="test${id}">${data[index].car}</td>`;
     inputtest3.innerHTML = `<td class="test3${id}">${c3.value}</td>`;
     inputtest2.innerHTML = `<td class="test2${id}">${c2.value}</td>`;
     inputtest.innerHTML = `<td class="test${id}">${c.value}</td>`;
-  }
-};
-
-const changedisable = (id) => {
-  let bbb = document.getElementById(`${id}`);
-  if (
-    ischangeage === false &&
-    ischangecar === false &&
-    ischangename === false
-  ) {
-    bbb.disabled = false;
-  } else {
-    bbb.disabled = true;
+    // window.location.reload();
   }
 };
 
 // 수정할때 검사할 함수들
 const checkChangename = (id) => {
-  let nameinput = document.querySelector(`.c3${id}`);
-  let b3 = document.getElementById(`${id}`);
-  console.log("nameinput", nameinput.value);
-
-  if (nameinput.value.length <= 0) {
-    ischangename = true;
-    b3.disabled = true; //비활성화
+  if (c3.value.length === 0) {
+    btnsss.disabled = true; //비활성화
   } else {
-    ischangename = false;
+    btnsss.disabled = false;
   }
-
-  changedisable(id);
 };
 
-const numberMaxLength2 = (e) => {
+const numberMaxLength2 = (e, id) => {
   //나이 수 제한
   if (e.value.length > e.maxLength) {
     e.value = e.value.slice(0, e.maxLength);
   }
-};
-const changeAGE = (id) => {
   const ageEle = document.querySelector(`.changecar2${id}`);
-  let b2 = document.getElementById(`${id}`);
-  let ageinput = document.querySelector(`.c2${id}`);
-  console.log("ageinput", ageinput.value);
-  if (ageinput.value >= 150 || ageinput.value < 0) {
+
+  if (c2.value >= 150 || c2.value < 0) {
     ageEle.textContent = "제대로 된 나이를 입력해주세요.";
+    btnsss.disabled = true; //비활성화
     ischangeage = true;
-    b2.disabled = true; //비활성화
   } else {
     ageEle.textContent = "";
     ischangeage = false;
+    btnsss.disabled = false;
   }
-  if (ageinput.value.length <= 0 && ageinput.value.length >= 3) {
-    ageEle.textContent = "";
-    ischangeage = true;
-    b2.disabled = true; //비활성화
+  if (c2.value.length === 0) {
+    btnsss.disabled = true; //비활성화
   }
-  changedisable(id);
 };
 
 const checkChangeCar = (id) => {
-  let b1 = document.getElementById(`${id}`);
-  let carinput = document.querySelector(`.c${id}`);
   const carEle = document.querySelector(`.changecar${id}`);
-  console.log("carinput", carinput.value);
-  if (carinput.value.length < 15 && carinput.value.length > 0) {
+  if (c.value.length < 15 && c.value.length > 0) {
     carEle.textContent = "경력은 15자리 이상 작성해주세요.";
     ischangecar = true;
-    b1.disabled = true; //비활성화
-  } else if (carinput.value.length === 0) {
+    btnsss.disabled = true; //비활성화
+  } else if (c.value.length === 0) {
     carEle.textContent = "";
     ischangecar = true;
-    b1.disabled = true; //비활성화
-  } else if (carinput.value.length >= 15) {
+    btnsss.disabled = true; //비활성화
+  } else if (c.value.length >= 15) {
     carEle.textContent = "";
     ischangecar = false;
+    btnsss.disabled = false;
   }
-  changedisable(id);
 };
-
 // 삭제
 const removeData = (id) => {
-  // 데이터덮어씌우기
-  let removebtn = document.getElementById(`${id}`);
-  let leftData = data.filter((item) => item.id !== String(id));
+  const leftData = data.filter((item) => item.id !== String(id));
   localStorage.setItem("userInfo", JSON.stringify(leftData));
-  let userLists = JSON.parse(localStorage.getItem("userInfo"));
-  data = [];
-  data.push(...userLists);
-  console.log(removebtn);
-  if (!removebtn) {
-    tbodys.innerHTML = "";
 
-    data.map((x, i) => {
-      tbodys.innerHTML += `
-    <tr>
-      <td>${x.name}</td>
-      <td>${x.age}</td>
-      <td class="test${x.id}">${x.car}</td>
-      <td>${x.nick}</td>
-      <td><div class="flexbtn"><button id="${x.id}" class="changebtn" onclick=changeData(${x.id})>수정</button><button class="changebtn" onclick=removeData(${x.id})>삭제</button></div></td>
-    </tr>
-  `;
-    });
-  } else {
-    let tr = document.querySelector(`.tablebody > tr[id=id${id}]`);
-    tr.remove();
-  }
+  //이거 class로는 안돼..?
+  let tr = document.querySelector(`.tablebody > tr[id=id${id}]`);
+  tr.remove();
+
+  // window.location.reload();
 };
